3dab46d99e6add6ebed6e273524a45d69d8f6fcf
Pratik Dubey <Pratikdubey7735@gmail.com>
2026-04-02 11:23:50 +0530

    deleted vercel.json

--- lib/api.ts
+++ lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 import { getToken } from "./auth"
 
-const BASE_URL = "http://localhost:8000"
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL
 
 const api = axios.create({
   baseURL: BASE_URL,
